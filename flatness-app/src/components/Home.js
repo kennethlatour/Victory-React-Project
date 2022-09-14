70f5eab5be03376290a578c194720eaf7ce80e32
Caleb <Tigers1025@gmail.com>
2022-09-14 11:51:04 -0400

    Added some content to home and about us

--- flatness-app/src/components/Home.js
+++ flatness-app/src/components/Home.js
@@ -3,8 +3,19 @@
 function Home(){
 
 return(
-    <div>
-    Home
+    <div className="home">
+    <h1>Welcome to our Flatness App!</h1>
+    <h3>This is a one stop shop for tracking your cardio workouts!</h3>
+    <p>We will help you keep track of several cardio workouts to include:</p>
+    <ul>
+        <li>Running</li>
+        <li>Walking</li>
+        <li>Swimming</li>
+        <li>Roller Blading</li>
+        <li>Biking</li>
+    </ul>
+    <h3>Flatness will keep track of the time you spent running, the distance of how far you ran, the date of the workout, and will calculate an approximate of how many calories you burned during the workout.</h3>
+
     </div>
 )
 
