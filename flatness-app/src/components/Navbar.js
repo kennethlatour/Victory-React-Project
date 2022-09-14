9b460f0d6c9365aaf2cd85e1885f35e513a9e2b0
Caleb <Tigers1025@gmail.com>
2022-09-13 20:19:59 -0400

    Working on Client Side Routing

--- flatness-app/src/components/Navbar.js
+++ flatness-app/src/components/Navbar.js
@@ -1,6 +1,19 @@
+import {Switch, Route, Link} from "react-router-dom"
+
 
 function Navbar(){
 
+    return(
+        <div>
+             <button>
+            <Link to='/'>
+            About Us
+            </Link>
+            </button>
+        <button>List of Workouts</button>
+        <button>Workout Form</button>
+        </div>
+    )
 
 }
 
